00a0e095c8301137545941ba3c599e71b1747a55
Juan Pablo <juampigarcia2014@gmail.com>
2021-08-25 20:41:29 -0300

    feat: Module Constants

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-let historyPlayer = {win: 0, lose: 0};
-let historyDealer = {win: 0, lose: 0};
-
 let hiddenCard;
-
-let playerBalance = 0;
-const bet = 100;
 
 // Player Model
 class Person {
@@ -30,9 +24,9 @@ class Person {
 
         this.hand.forEach(card => {
             score += card.v;
-            aceCounter = (card.v === 11) ? aceCounter += 1 : aceCounter;
+            aceCounter = (card.v === ACE_VALUE) ? aceCounter += 1 : aceCounter;
 
-            while (score > 21 && aceCounter > 0) {
+            while (score > MAX_VALUE && aceCounter > 0) {
                 score -= 10;
                 aceCounter -= 1;
             }
@@ -65,76 +59,22 @@ class GameModel {
     constructor() {
         this.player = new Person();
         this.dealer = new Person();
-        this.deck = [
-            {k: "c1", v: 11},
-            {k: "c2", v: 2},
-            {k: "c3", v: 3},
-            {k: "c4", v: 4},
-            {k: "c5", v: 5},
-            {k: "c6", v: 6},
-            {k: "c7", v: 7},
-            {k: "c8", v: 8},
-            {k: "c9", v: 9},
-            {k: "c10", v: 10},
-            {k: "c11", v: 10},
-            {k: "c12", v: 10},
-            {k: "c13", v: 10},
-            {k: "h1", v: 11},
-            {k: "h2", v: 2},
-            {k: "h3", v: 3},
-            {k: "h4", v: 4},
-            {k: "h5", v: 5},
-            {k: "h6", v: 6},
-            {k: "h7", v: 7},
-            {k: "h8", v: 8},
-            {k: "h9", v: 9},
-            {k: "h10", v: 10},
-            {k: "h11", v: 10},
-            {k: "h12", v: 10},
-            {k: "h13", v: 10},
-            {k: "s1", v: 11},
-            {k: "s2", v: 2},
-            {k: "s3", v: 3},
-            {k: "s4", v: 4},
-            {k: "s5", v: 5},
-            {k: "s6", v: 6},
-            {k: "s7", v: 7},
-            {k: "s8", v: 8},
-            {k: "s9", v: 9},
-            {k: "s10", v: 10},
-            {k: "s11", v: 10},
-            {k: "s12", v: 10},
-            {k: "s13", v: 10},
-            {k: "d1", v: 11},
-            {k: "d2", v: 2},
-            {k: "d3", v: 3},
-            {k: "d4", v: 4},
-            {k: "d5", v: 5},
-            {k: "d6", v: 6},
-            {k: "d7", v: 7},
-            {k: "d8", v: 8},
-            {k: "d9", v: 9},
-            {k: "d10", v: 10},
-            {k: "d11", v: 10},
-            {k: "d12", v: 10},
-            {k: "d13", v: 10}
-        ];
     }
 
     shuffle(value) {
         let i;
         for (i = 0; i < value; i++) {
-            this.deck.sort(() => Math.random() - 0.5);
+            DECK.sort(() => Math.random() - 0.5);
         }
     }
 
     dealInitialCards() {
         for (let i = 0; i < 2; i++) {
-            this.player.hand = this.deck.pop();
+            this.player.hand = DECK.pop();
             if (this.dealer.hand.length === 0) {
-                this.dealer.hand = this.deck.pop();
+                this.dealer.hand = DECK.pop();
             } else {
-                this.dealer.hiddenCard = this.deck.pop();
+                this.dealer.hiddenCard = DECK.pop();
             }
 
         }
@@ -149,21 +89,21 @@ function start() {
     initial(false);
 
     // Event RESTART
-    let restart = document.getElementById("restartButton");
-    restart.addEventListener("click", (() => {
+    let restart = document.getElementById(RESTART_BUTTON);
+    restart.addEventListener(CLICK, (() => {
         if (!gameModel.player.active) {
             initial();
         }
     }));
 
     // Event HIT
-    let hit = document.getElementById("hitButton");
-    hit.addEventListener("click", (() => {
+    let hit = document.getElementById(HIT_BUTTON);
+    hit.addEventListener(CLICK, (() => {
         if (!gameModel.player.active) {
             return
         }
 
-        let card = gameModel.deck.pop();
+        let card = DECK.pop();
         gameModel.player.hand.push(card);
 
         cardVisualizer(gameModel.player);
@@ -173,8 +113,8 @@ function start() {
     }));
 
     // Event STAND
-    let stand = document.getElementById("standButton");
-    stand.addEventListener("click", (() => {
+    let stand = document.getElementById(STAND_BUTTON);
+    stand.addEventListener(CLICK, (() => {
 
         if (!gameModel.player.active) {
             return
@@ -186,9 +126,9 @@ function start() {
         gameModel.dealer.hand.push(hiddenCard);
 
         // DEALER PRETEND WIN
-        for (let i = 0; i < gameModel.deck.length; i++) {
-            if (gameModel.dealer.score <= 16 || gameModel.dealer.score < gameModel.player.score) {
-                let card = gameModel.deck.pop()
+        for (let i = 0; i < DECK.length; i++) {
+            if (gameModel.dealer.score <= STAND_DEALER_VALUE || gameModel.dealer.score < gameModel.player.score) {
+                let card = DECK.pop()
                 gameModel.dealer.hand.push(card);
             } else {
                 break;
@@ -205,31 +145,30 @@ function start() {
     function initial(reset = true) {
 
         gameModel = new GameModel();
-        gameModel.shuffle(4);
+        gameModel.shuffle(SHUFFLE_VALUE);
         gameModel.dealInitialCards();
 
         if (!reset) {
             do {
-                playerBalance = prompt("Enter the balance you wish to bet, consider that each bet will be $100.");
-                if (isNaN(Number(playerBalance)) || playerBalance < 100) {
-                    playerBalance = 0;
-                    alert("The balance must be greater than or equal to 100, please re-enter the balance.");
+                PLAYER_BALANCE = prompt(BALANCE_PROMPT);
+                if (isNaN(Number(PLAYER_BALANCE)) || PLAYER_BALANCE < BET_VALUE) {
+                    alert(BALANCE_ALERT);
                 }
-            } while (playerBalance < 100)
+            } while (PLAYER_BALANCE < BET_VALUE)
         }
 
-        if (playerBalance >= 100) {
-            playerBalance -= bet;
+        if (PLAYER_BALANCE >= BET_VALUE) {
+            PLAYER_BALANCE -= BET_VALUE;
         } else {
             initial(false);
         }
 
-        setText("result", "Playing...");
+        setText(RESULT_ID, PLAYING);
         cardVisualizer(gameModel.player, false, true);
         cardVisualizer(gameModel.dealer);
         cardVisualizer(gameModel.dealer, true);
 
-        if (gameModel.player.score === 21) {
+        if (gameModel.player.score === MAX_VALUE) {
             checkResult(true);
         }
 
@@ -245,21 +184,21 @@ function start() {
             hiddenCard = gameModel.dealer.hiddenCard.pop()
             gameModel.dealer.hand.push(hiddenCard)
             cardVisualizer(gameModel.dealer)
-            historyPlayer.win += 1;
-            historyDealer.lose += 1;
+            HISTORY_PLAYER.win += 1;
+            HISTORY_DEALER.lose += 1;
             winBet(false, true);
-            setText("result", "<img src=\"img/blackjack.png\">");
+            setText(RESULT_ID, BLACK_JACK);
             showScore()
             return
         }
 
 
         // DEALER WIN
-        if (gameModel.dealer.score >= gameModel.player.score && gameModel.dealer.score <= 21) {
+        if (gameModel.dealer.score >= gameModel.player.score && gameModel.dealer.score <= MAX_VALUE) {
             dealerWin = true;
         }
 
-        if (gameModel.player.score > 21) {
+        if (gameModel.player.score > MAX_VALUE) {
             dealerWin = true;
             gameModel.player.active = false;
             hiddenCard = gameModel.dealer.hiddenCard.pop();
@@ -272,15 +211,15 @@ function start() {
         // SHOW TEXT
         if (!(gameModel.player.active)) {
             if (dealerWin) {
-                setText("result", "The DEALER won this round!")
-                historyDealer.win += 1;
-                historyPlayer.lose += 1;
+                setText(RESULT_ID, DEALER_WIN)
+                HISTORY_DEALER.win += 1;
+                HISTORY_PLAYER.lose += 1;
                 winBet(true, false);
                 showScore();
             } else {
-                setText("result", "The PLAYER won this round!")
-                historyPlayer.win += 1;
-                historyDealer.lose += 1;
+                setText(RESULT_ID, PLAYER_WIN)
+                HISTORY_PLAYER.win += 1;
+                HISTORY_DEALER.lose += 1;
                 winBet(false, false);
                 showScore();
             }
@@ -303,8 +242,8 @@ function start() {
 
 
         // Create image element
-        let entityElement = document.createElement('img');
-        entityElement.src = `img/hidden-cards/hidden.png`
+        let entityElement = document.createElement(IMG_ELEMENT);
+        entityElement.src = HIDDEN_CARD;
 
         // Show Cards for entity
         if (!(hidden)) {
@@ -316,8 +255,8 @@ function start() {
                 dealerSelector.innerHTML = '';
             }
             entity.hand.forEach(card => {
-                let entityElement = document.createElement('img');
-                entityElement.src = `img/cards/${card.k}.png`
+                let entityElement = document.createElement(IMG_ELEMENT);
+                entityElement.src = CARD(card);
 
                 if (entity === gameModel.player) {
                     playerSelector.appendChild(entityElement)
@@ -335,10 +274,10 @@ function start() {
     // Adding and subtracting balance
     function winBet(dealerWin = false, blackjack = false) {
         if (blackjack) {
-            playerBalance += bet * 2.5;
+            PLAYER_BALANCE += BET_VALUE * 2.5;
         } else {
             if (!dealerWin) {
-                playerBalance += bet * 2;
+                PLAYER_BALANCE += BET_VALUE * 2;
             }
 
         }
@@ -347,15 +286,18 @@ function start() {
     function showScore() {
         setText("scorePlayer", gameModel.player.score)
         setText("scoreDealer", gameModel.dealer.score)
-        setText("winDealer", `Win: ${historyDealer.win}`);
-        setText("loseDealer", `Lose: ${historyDealer.lose}`);
-        setText("winPlayer", `Win: ${historyPlayer.win}`);
-        setText("losePlayer", `Lose: ${historyPlayer.lose}`);
-        setText("dealerBalance", `Balance: 999999 $`);
-        setText("playerBalance", `Balance: ${playerBalance} $`);
+        setText("winDealer", `Win: ${HISTORY_DEALER.win}`)
+        setText("loseDealer", `Lose: ${HISTORY_DEALER.lose}`)
+        setText("winPlayer", `Win: ${HISTORY_PLAYER.win}`)
+        setText("losePlayer", `Lose: ${HISTORY_PLAYER.lose}`)
+        setText("dealerBalance", `Balance: 999999 $`)
+        setText("playerBalance", `Balance: ${PLAYER_BALANCE} $`)
+
     }
 }
 
 function setText(id, text) {
     document.getElementById(id).innerHTML = text;
 }
+
+window.addEventListener('load', start);
